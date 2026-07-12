6dbbc1b262f12a6a2dec40e7e38d4549e66efabe
ABDEL MONIM MAZGOURA <mazgouraabdalmounim@gmail.com>
2026-07-12 20:56:01 +0100

    fix: remove unused env import and update database URL fallback in prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_dummy@ep-dummy-pooler.us-east-1.aws.neon.tech/neondb",
   },
 });
